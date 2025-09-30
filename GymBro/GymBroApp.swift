ca72554f69bad20d0bbbc77e0319b65024289508
Samuel Martins <fsamuelsmartins@gmail.com>
2025-09-30 11:49:53 -0300

    Initial Commit

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by Samuel Martins on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
