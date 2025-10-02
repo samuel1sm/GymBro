db5c128f3ae46bb5cf3694426740ffa1779a9bd1
Samuel Martins <fsamuelsmartins@gmail.com>
2025-10-02 16:40:50 -0300

    adding action when textfield was clicked

--- GymBro/WorkoutExpirience/Components/DeafaultTextField.swift
+++ GymBro/WorkoutExpirience/Components/DeafaultTextField.swift
@@ -5,12 +5,20 @@ struct DeafaultTextField: View {
 	@Binding var text: String
 	let keyboardType: UIKeyboardType
 	var placeholder: String
+	let selectableAction: (() -> Void)?
 
-	init(title: String, text: Binding<String>, keyboardType: UIKeyboardType = .default, placeholder: String) {
+	init(
+		title: String,
+		text: Binding<String>,
+		keyboardType: UIKeyboardType = .default,
+		placeholder: String,
+		selectableAction: (() -> Void)? = nil
+	) {
 		self.title = title
 		self._text = text
 		self.keyboardType = keyboardType
 		self.placeholder = placeholder
+		self.selectableAction = selectableAction
 	}
 
 	var body: some View {
@@ -18,20 +26,28 @@ struct DeafaultTextField: View {
 			Text(title)
 				.bold()
 				.font(.subheadline)
-			VStack {
+			HStack {
 				TextField(
 					"",
 					text: $text,
 					prompt: Text(placeholder).foregroundColor(.gray)
 				)
+				.disabled(selectableAction != nil)
 				.keyboardType(keyboardType)
 				.padding(.leading, 2)
 				.padding(8)
+				if selectableAction != nil {
+					Image(systemName: "arrowtriangle.down.fill")
+						.padding(.horizontal, 16)
+						.foregroundColor(.gray)
+				}
 			}
 			.background {
 				RoundedRectangle(cornerRadius: 6)
 					.fill(Color(uiColor: .systemGray6))
 			}
+		}.onTapGesture {
+			selectableAction?()
 		}
 	}
 }
@@ -39,9 +55,8 @@ struct DeafaultTextField: View {
 #Preview {
 	DeafaultTextField(
 		title: "Title",
-		text: .constant(
-			"Text"
-		),
-		placeholder: "Placeholder"
+		text: .constant(""),
+		placeholder: "Placeholder",
+		selectableAction: nil
 	)
 }
