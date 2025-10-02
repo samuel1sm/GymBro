63885129aca1aadc50a87ad55467cf25c76b788b
Samuel Martins <fsamuelsmartins@gmail.com>
2025-10-02 14:50:57 -0300

    adding set counter to bar view

--- GymBro/WorkoutExpirience/Components/LoadingBarView.swift
+++ GymBro/WorkoutExpirience/Components/LoadingBarView.swift
@@ -2,40 +2,49 @@ import SwiftUI
 
 struct LoadingBarView<Stage: StageFlow>: View {
 	let stage: Stage
+	let hasStepCounter: Bool
 
 	@State private var totalWidth: CGFloat = 1
 	@State private var loadedWidth: CGFloat = 0.0
 
 	var body: some View {
-		ZStack(alignment: .leading) {
-			GeometryReader { geometry in
+		VStack(alignment: .leading, spacing: 12) {
+			ZStack(alignment: .leading) {
+				GeometryReader { geometry in
+					Rectangle()
+						.clipShape(.capsule)
+						.opacity(0.8)
+						.foregroundStyle(.gray)
+						.onAppear {
+							totalWidth = geometry.size.width
+							updateWidth()
+						}
+						.onChange(of: geometry.size.width) { _, newWidth in
+							totalWidth = newWidth
+							updateWidth()
+						}
+				}
+				
 				Rectangle()
 					.clipShape(.capsule)
 					.opacity(0.8)
-					.foregroundStyle(.gray)
-					.onAppear {
-						totalWidth = geometry.size.width
-						updateWidth()
-					}
-					.onChange(of: geometry.size.width) { _, newWidth in
-						totalWidth = newWidth
-						updateWidth()
-					}
+					.foregroundStyle(.black)
+					.frame(width: loadedWidth)
+					.animation(.easeInOut(duration: 0.35), value: loadedWidth)
+			}
+			.frame(height: 10)
+			.onChange(of: stage) { _, _ in
+				updateWidth()
+			}
+			.onAppear {
+				updateWidth()
 			}
 
-			Rectangle()
-				.clipShape(.capsule)
-				.opacity(0.8)
-				.foregroundStyle(.black)
-				.frame(width: loadedWidth)
-				.animation(.easeInOut(duration: 0.35), value: loadedWidth)
-		}
-		.frame(height: 10)
-		.onChange(of: stage) { _, _ in
-			updateWidth()
-		}
-		.onAppear {
-			updateWidth()
+			if hasStepCounter {
+				Text("Step \(stage.rawValue) of \(Stage.allCases.count)")
+					.font(.subheadline)
+					.foregroundStyle(.gray)
+			}
 		}
 	}
 
@@ -46,5 +55,5 @@ struct LoadingBarView<Stage: StageFlow>: View {
 }
 
 #Preview {
-	LoadingBarView(stage: CustomisationStages.personalInformations)
+	LoadingBarView(stage: CustomisationStages.personalInformations, hasStepCounter: true)
 }
